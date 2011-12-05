423ac3efb50d4e3c5a76feb17c9cdde2de7cb2af
Erik Kastner <kastner@gmail.com>
2011-12-05 12:06:27 -0500

    initial import

---
+++ content_script.js
@@ -0,0 +1,17 @@
+function refreshMenu() {
+  chrome.contextMenus.removeAll();
+
+  var imageMenu = chrome.contextMenus.create({
+    "title": "Pin It!", 
+    "contexts": ["image"], "onclick": function (info, tab) {
+      console.log(info);
+      console.log(tab);
+      var pinUrl = "http://pinterest.com/pin/create/bookmarklet/";
+      pinUrl += "?media=" + escape(info.srcUrl) + "&url=" + escape(tab.url) + "&alt=alt&title=foo"; //+ escape(tab.title);
+      pinUrl += "&is_video=false&";
+      window.open(pinUrl,"pin"+(new Date).getTime(),"status=no,resizable=no,scrollbars=yes,personalbar=no,directories=no,location=no,toolbar=no,menubar=no,width=632,height=270,left=0,top=0");
+    }
+  });
+}
+
+refreshMenu();
