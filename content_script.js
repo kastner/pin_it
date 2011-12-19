6a6d810be312884acb30f0c586d61a11c32fb762
Erik Kastner <kastner@gmail.com>
2011-12-19 00:12:31 -0500

    trying to resize the window for britt

--- content_script.js
+++ content_script.js
@@ -9,7 +9,7 @@ function refreshMenu() {
       var pinUrl = "http://pinterest.com/pin/create/bookmarklet/";
       pinUrl += "?media=" + escape(info.srcUrl) + "&url=" + escape(tab.url) + "&alt=alt&title=foo"; //+ escape(tab.title);
       pinUrl += "&is_video=false&";
-      window.open(pinUrl,"pin"+(new Date).getTime(),"status=no,resizable=no,scrollbars=yes,personalbar=no,directories=no,location=no,toolbar=no,menubar=no,width=632,height=270,left=0,top=0");
+      window.open(pinUrl,"pin"+(new Date).getTime(),"status=no,resizable=no,scrollbars=yes,personalbar=no,directories=no,location=no,toolbar=no,menubar=no,width=635,height=290,left=0,top=0");
     }
   });
 }
